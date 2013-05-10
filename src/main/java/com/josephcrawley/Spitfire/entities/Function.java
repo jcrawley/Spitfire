c870156687dc014549b8ec7ee659fbe51a9939ff
Joe Crawley <joe.crawley10@gmail.com>
2013-05-09 18:32:46 -0700

    resolving merge

--- src/main/java/com/josephcrawley/Spitfire/entities/Function.java
+++ src/main/java/com/josephcrawley/Spitfire/entities/Function.java
@@ -22,7 +22,7 @@ public class Function extends Subroutine {
 
     @Override
     public void analyzeSignature(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
-        // TODO: Analyze type
+        
         super.analyzeSignature(log, table, owner, inLoop);
     }
 }
